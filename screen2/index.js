c2cc836c359b2e9e2052c23f56ab236b40cfbdcc
dev-assassin <alexandra.uefa@gmail.com>
2020-10-06 03:21:34 +0300

    frontend work

---
+++ screen2/index.js
@@ -0,0 +1,15 @@
+function viewPassword()
+{
+    var passwordInput = document.getElementById('inputPassword');
+    var passStatus = document.getElementById('pass-status');
+    
+    if (passwordInput.type == 'password'){
+        passwordInput.type='text';
+        passStatus.className='fa fa-eye-slash';
+        
+    }
+    else{
+        passwordInput.type='password';
+        passStatus.className='fa fa-eye';
+    }
+}
